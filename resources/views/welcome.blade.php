fc78f038cb2b9a04494e192b0286f6371699c5d9
kouser-ahamed <kouserahamed.cse.diu@gmail.com>
2025-05-28 12:41:04 +0600

    Basic Design Portfolio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello From Laraval</title>
+    <title>Full-Stack-Dynamic-Portfolio</title>
 </head>
 <body>
-    <h1>
-        Hello From Laraval
-    </h1>
+    <div>
+        <div>Kouser</div>
+        <div>About Me</div>
+        <div>Work</div>
+        <div>Protfolio</div>
+        <div>Services</div>
+        <div>Pring</div>
+        <div><button>Contact Me</button></div>
+    
+    </div>
+    <hr>
+
+    <div>
+        <div>
+            <h2> Hi I am Kouser Ahamed</h2>
+            <p>Expert Visual Designer</p>
+            <button>Know Me</button>
+            <button>See My Project</button>
+
+        </div>
+        <div>
+            <p> <img src="assets/images/kou.jpg" alt=""></p>
+        </div>
+    </div>
+    <hr>
+    <div>
+        <div>LinkedId</div>
+        <div>Dribble</div>
+        <div>Behance</div>
+    </div>
+    <hr>
+    <div>
+        <div><button>Website Design</button></div>
+        <div><button>Deshoard Design</button></div>
+        <div><button>Mobile App Design</button></div>
+    </div>
+
+    
     
 </body>
 </html>
